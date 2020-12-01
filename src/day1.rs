469fa0214e1a4b8e16fa3830487f63fef7fec1ea
Kevin Clark <kevin.clark@gmail.com>
2020-12-01 15:20:17 -0800

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -13,19 +13,41 @@ pub fn two_sum(total: i64, list: &[i64]) -> Option<(i64, i64)> {
     None
 }
 
+pub fn three_sum(total: i64, list: &[i64]) -> Option<(i64, i64, i64)> {
+    for (idx, n) in list.iter().enumerate() {
+        if let Some((second, third)) = two_sum(total - n, &list[idx + 1..]) {
+            return Some((*n, second, third));
+        }
+    }
+
+    None
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn with_complement() {
+    fn two_sum_with_complement() {
         let result = two_sum(2020, &[1721, 979, 366, 299, 675, 1456]);
         assert_eq!(Some((1721, 299)), result);
     }
 
     #[test]
-    fn without_complement() {
+    fn two_sum_without_complement() {
         let result = two_sum(10, &[1721, 979, 366, 299, 675, 1456]);
+        assert_eq!(None, result);
+    }
+
+    #[test]
+    fn three_sum_with_complement() {
+        let result = three_sum(2020, &[1721, 979, 366, 299, 675, 1456]);
+        assert_eq!(Some((979, 366, 675)), result);
+    }
+
+    #[test]
+    fn threetwo_sum_without_complement() {
+        let result = three_sum(10, &[1721, 979, 366, 299, 675, 1456]);
         assert_eq!(None, result);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use aoc2020::day1::two_sum;
+use aoc2020::day1::*;
 use std::fs;
 
 fn main() {
-    let input = &fs::read_to_string("input/1.txt").unwrap();
-    match two_sum(
-        2020,
-        &input
-            .trim()
-            .split('\n')
-            .map(|s| s.parse::<i64>().unwrap())
-            .collect::<Vec<i64>>(),
-    ) {
+    let input = &fs::read_to_string("input/1.txt")
+        .unwrap()
+        .trim()
+        .split('\n')
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    println!("Part 1\n");
+    match two_sum(2020, &input) {
         Some(result) => println!(
             "Two numbers: {} {} and their product: {}",
             result.0,
             result.1,
             result.0 * result.1
+        ),
+        None => println!("No result!"),
+    }
+
+    println!("\nPart 2\n");
+    match three_sum(2020, &input) {
+        Some(result) => println!(
+            "Three numbers: {} {} {} and their product: {}",
+            result.0,
+            result.1,
+            result.2,
+            result.0 * result.1 * result.2
         ),
         None => println!("No result!"),
     }
