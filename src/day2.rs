6a68d3588d796c92f26754ee8cfa8dfbadfeaffb
Kevin Clark <kevin.clark@gmail.com>
2020-12-01 23:27:17 -0800

    Day 2 part 1
    Still has a warning

---
+++ src/day2.rs
@@ -0,0 +1,51 @@
+use std::str;
+
+pub fn number_of_valid_passwords(input: &str) -> usize {
+    // N-N L: pass
+    input
+        .lines()
+        .filter(|line| {
+            let mut chars = line.chars();
+
+            let start = chars
+                .by_ref()
+                .take_while(|c| *c != '-')
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+
+            let end = chars
+                .by_ref()
+                .take_while(|c| *c != ' ')
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+
+            let range = start..=end;
+
+            let letter = chars.next().unwrap();
+            chars.by_ref().take(2);
+
+            let pass: String = chars.collect();
+
+            let occurences = pass.chars().filter(|l| *l == letter).count();
+
+            range.contains(&occurences)
+        })
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn a_match() {
+        assert_eq!(1, number_of_valid_passwords(&"1-3 a: abcde"));
+    }
+
+    #[test]
+    fn no_match() {
+        assert_eq!(0, number_of_valid_passwords(&"1-3 x: abcde"));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use aoc2020::day1::*;
+use aoc2020::day1;
+use aoc2020::day2;
 use clap::App;
 use std::fs;
 
 fn main() {
     let matches = App::new("aoc2020")
         .arg_from_usage("--day1... 'runs the day 1 solution'")
+        .arg_from_usage("--day2... 'runs the day 2 solution'")
         .get_matches();
 
     if matches.is_present("day1") {
         solve_day1()
+    }
+
+    if matches.is_present("day2") {
+        solve_day2()
     }
 }
 
@@ -23,7 +29,7 @@ fn solve_day1() {
         .collect::<Vec<i64>>();
 
     println!("## Part 1\n");
-    match two_sum(2020, &input) {
+    match day1::two_sum(2020, &input) {
         Some(result) => println!(
             "Two numbers: {} {} and their product: {}",
             result.0,
@@ -34,7 +40,7 @@ fn solve_day1() {
     }
 
     println!("\n## Part 2\n");
-    match three_sum(2020, &input) {
+    match day1::three_sum(2020, &input) {
         Some(result) => println!(
             "Three numbers: {} {} {} and their product: {}",
             result.0,
@@ -44,4 +50,13 @@ fn solve_day1() {
         ),
         None => println!("No result!"),
     }
+}
+
+fn solve_day2() {
+    println!("# Day 2\n");
+
+    let input = &fs::read_to_string("input/2.txt").unwrap();
+
+    println!("## Part 1\n");
+    println!("{}", day2::number_of_valid_passwords(input));
 }
