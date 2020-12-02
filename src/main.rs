a4fe82c2d7c57748cba65a6aa9efd0ccaedc497f
Kevin Clark <kevin.clark@gmail.com>
2020-12-01 22:02:04 -0800

    Setup for multiple days

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use aoc2020::day1::*;
+use clap::App;
 use std::fs;
 
 fn main() {
+    let matches = App::new("aoc2020")
+        .arg_from_usage("--day1... 'runs the day 1 solution'")
+        .get_matches();
+
+    if matches.is_present("day1") {
+        solve_day1()
+    }
+}
+
+fn solve_day1() {
+    println!("# Day 1\n");
+
     let input = &fs::read_to_string("input/1.txt")
         .unwrap()
         .trim()
@@ -9,7 +22,7 @@ fn main() {
         .map(|s| s.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
 
-    println!("Part 1\n");
+    println!("## Part 1\n");
     match two_sum(2020, &input) {
         Some(result) => println!(
             "Two numbers: {} {} and their product: {}",
@@ -20,7 +33,7 @@ fn main() {
         None => println!("No result!"),
     }
 
-    println!("\nPart 2\n");
+    println!("\n## Part 2\n");
     match three_sum(2020, &input) {
         Some(result) => println!(
             "Three numbers: {} {} {} and their product: {}",
