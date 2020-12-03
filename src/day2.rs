91d644896b18d08ad8127df7f0966fa7f8f7a23f
Kevin Clark <kevin.clark@gmail.com>
2020-12-02 17:58:39 -0800

    day 2 bug introduced when optimizing

--- src/day2.rs
+++ src/day2.rs
@@ -33,7 +33,7 @@ pub fn number_of_valid_part_2_passwords(input: &[u8]) -> usize {
                 let (start, end, letter, pass) = parse(&line);
 
                 let first = pass[start - 1];
-                let second = pass[end - start];
+                let second = pass[end - 1];
 
                 let first_match = first == letter;
                 let second_match = second == letter;
@@ -53,6 +53,7 @@ pub fn number_of_valid_part_1_passwords(input: &[u8]) -> usize {
                 false
             } else {
                 let (start, end, letter, pass) = parse(&line);
+
                 let range = start..=end;
                 let occurences = pass.iter().filter(|c| *c == &letter).count();
 
@@ -92,6 +93,16 @@ mod tests {
                 &"1-3 a: abcde\n1-3 b: abcde\n5-10 a: abcde\n".as_bytes()
             )
         );
+
+        assert_eq!(
+            2,
+            number_of_valid_part_1_passwords(
+                &"1-3 a: abcde\n\
+                                                         1-3 b: cdefg\n\
+                                                         2-9 c: ccccccccc\n"
+                    .as_bytes()
+            )
+        );
     }
 
     #[test]
@@ -107,6 +118,17 @@ mod tests {
         assert_eq!(
             0,
             number_of_valid_part_2_passwords(&"1-3 x: cabde".as_bytes())
+        );
+
+        assert_eq!(
+            2,
+            number_of_valid_part_2_passwords(
+                &"1-3 a: abcde\n\
+                                                         1-3 b: cdefg\n\
+                                                         2-4 b: cdebg\n\
+                                                         2-9 c: ccccccccc\n"
+                    .as_bytes()
+            )
         );
     }
 }
